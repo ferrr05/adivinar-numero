936eb578e1f628829490c23c89867b627572155c
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-05-11 10:48:26 +0200

    adivinar número

---
+++ script.js
@@ -0,0 +1,29 @@
+const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
+let intentos = 0;
+function pedirNumero() {
+    const numero = prompt('Introduce un número entre 1 y 15, tienes tres intentos');
+    return parseInt(numero);
+}
+function comprobarNumero(numero) {
+    intentos++;
+
+    if (numero === numeroAdivinar) {
+        alert(`¡Has acertado el número en ${intentos} intentos.`);
+        return true;
+    } else if (numero < numeroAdivinar) {
+        alert('El número que has introducido es menor.');
+    } else {
+        alert('El número que has introducido es mayor.');
+    }
+    if (intentos === 3 && numero != numeroAdivinar) {
+        alert(`Has perdido, el numero era ${numeroAdivinar}.`);
+    }
+
+    return false;
+}
+for (let i = 0; i < 3; i++) {
+    const numero = pedirNumero();
+    if (comprobarNumero(numero)) {
+        break;
+    }
+}
