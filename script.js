ad8ec0cd133a6166e013be30a10deaee9ec8d161
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-05-11 10:55:28 +0200

    mensajes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
 let intentos = 0;
 function pedirNumero() {
-    const numero = prompt('Introduce un número entre 1 y 15, tienes tres intentos');
+    const numero = prompt('Tienes que adivinar el número que estoy pensando del 1 al 15, tienes tres intentos');
     return parseInt(numero);
 }
 function comprobarNumero(numero) {
     intentos++;
 
     if (numero === numeroAdivinar) {
-        alert(`¡Has acertado el número en ${intentos} intentos.`);
+        alert(`¡Muy bien, solo has necesitado ${intentos} intentos.`);
         return true;
     } else if (numero < numeroAdivinar) {
-        alert('El número que has introducido es menor.');
+        alert('El número que has introducido es menor al que estoy pensando.');
     } else {
-        alert('El número que has introducido es mayor.');
+        alert('El número que has introducido es mayor al que estoy pensando.');
     }
     if (intentos === 3 && numero != numeroAdivinar) {
-        alert(`Has perdido, el numero era ${numeroAdivinar}.`);
+        alert(`Lo siento, no has adivinado el número, el numero era ${numeroAdivinar}.`);
     }
 
     return false;
